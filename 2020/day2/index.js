25924fb3ef0cf4275e3a358dbca154d41bff3221
Kai Wood <kai.wood@vitagroup.ag>
2020-12-02 14:57:06 +0100

    Day 2

---
+++ 2020/day2/index.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+
+const database = fs
+  .readFileSync("./input.txt", "utf8")
+  .trim()
+  .split("\n")
+  .map((entry) => {
+    const [_, min, max, char, password] = entry.match(
+      /(\d+)-(\d+) (\w): (\w+)/
+    );
+
+    return {
+      min: Number(min),
+      max: Number(max),
+      char,
+      password,
+    };
+  });
+
+console.log(database);
+
+let validPart1 = 0;
+let validPart2 = 0;
+
+// Solution part 1
+for (let policy of database) {
+  const numberOfChars = policy.password
+    .split("")
+    .filter((p) => p === policy.char).length;
+
+  if (numberOfChars >= policy.min && numberOfChars <= policy.max) {
+    validPart1++;
+  }
+}
+
+// Solution part 2
+for (let policy of database) {
+  const firstChar = policy.password[policy.min - 1];
+  const secondChar = policy.password[policy.max - 1];
+
+  if (firstChar === secondChar) continue;
+
+  if (firstChar === policy.char || secondChar === policy.char) {
+    validPart2++;
+  }
+}
+
+console.log(validPart1);
+console.log(validPart2);
